d5b5c821fbd39001cf24afa744ba300370940bc9
r3n33 <renee1382@gmail.com>
2015-04-27 10:32:40 -0500

    Fix for issue #4 on master branch

--- src/hexapod_locomotion/src/control.cpp
+++ src/hexapod_locomotion/src/control.cpp
@@ -40,6 +40,9 @@ Control::Control( void )
     hex_state_ = false;
     imu_init_stored_ = false;
     imu_override_.active = false;
+    imu_pitch_lowpass_ = 0;
+    imu_roll_lowpass_ = 0;
+    imu_yaw_lowpass_ = 0;
     base_.y = 0.0;
     base_.x = 0.0;
     base_.yaw = 0.0;
